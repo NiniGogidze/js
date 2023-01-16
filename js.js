f622f7fed9fe273fc43c350aa75b0679d5d8e686
NiniGogidze <119741770+NiniGogidze@users.noreply.github.com>
2023-01-17 03:30:48 +0400

    Add files via upload

---
+++ js.js
@@ -0,0 +1,21 @@
+let name = "giorgi";
+let surname = "kiparoidze";
+let age = 18;
+
+console.log(name + " " + surname);
+console.log(`${name} ${surname}`);
+
+
+let x = 90;
+let y = 10;
+
+let sum = x + y;
+let div = x % y;
+let sub = x - y;
+let mul = x * y;
+
+
+let a = 5;
+let b = 7;
+
+let log = a * b;
